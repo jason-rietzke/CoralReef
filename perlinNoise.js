6f675c4892551ebea787f161b2bcd100690dcab2
Jason Rietzke <jason.rietzke@livereader.com>
2021-04-10 19:07:20 +0200

    Reimplemented PerlinNoise
    + changes architecture

--- perlinNoise.js
+++ perlinNoise.js
@@ -1,49 +1,55 @@
 'use strict';
 
-let PerlinNoise = {
+class PerlinNoise {
 
-	rand_vect: function(){
+	constructor() {
+		this.gradients = {};
+	}
+
+	randomVector() {
 		let theta = Math.random() * 2 * Math.PI;
-		return {x: Math.cos(theta), y: Math.sin(theta)};
-	},
+		return {
+			x: Math.cos(theta), 
+			y: Math.sin(theta)
+		};
+	}
 
-	dot_prod_grid: function(x, y, vx, vy){
+	smootherstep(x) {
+		return 6*x**5 - 15*x**4 + 10*x**3;
+	}
+
+	dotProdGrid(x, y, vx, vy) {
 		let g_vect;
 		let d_vect = {x: x - vx, y: y - vy};
 		if (this.gradients[[vx,vy]]){
 			g_vect = this.gradients[[vx,vy]];
 		} else {
-			g_vect = this.rand_vect();
+			g_vect = this.randomVector();
 			this.gradients[[vx, vy]] = g_vect;
 		}
 		return d_vect.x * g_vect.x + d_vect.y * g_vect.y;
-	},
+	}
 
-	smootherstep: function(x){
-		return 6*x**5 - 15*x**4 + 10*x**3;
-	},
+	interpolate(x, a, b) {
+		return a + this.smootherstep(x) * (b - a);
+	}
 
-	interp: function(x, a, b){
-		return a + this.smootherstep(x) * (b-a);
-	},
-
-	seed: function(){
+	seed() {
 		this.gradients = {};
-	},
+	}
 
-	get: function(x, y) {
+	get(x, y) {
 		let xf = Math.floor(x);
 		let yf = Math.floor(y);
 		//interpolate
-		let tl = this.dot_prod_grid(x, y, xf,   yf);
-		let tr = this.dot_prod_grid(x, y, xf+1, yf);
-		let bl = this.dot_prod_grid(x, y, xf,   yf+1);
-		let br = this.dot_prod_grid(x, y, xf+1, yf+1);
+		let tl = this.dotProdGrid(x, y, xf,   yf);
+		let tr = this.dotProdGrid(x, y, xf+1, yf);
+		let bl = this.dotProdGrid(x, y, xf,   yf+1);
+		let br = this.dotProdGrid(x, y, xf+1, yf+1);
 		let xt = this.interp(x-xf, tl, tr);
 		let xb = this.interp(x-xf, bl, br);
 		let v = this.interp(y-yf, xt, xb);
 		return v;
 	}
-}
 
-PerlinNoise.seed();
+}
